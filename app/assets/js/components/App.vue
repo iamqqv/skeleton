e8d8bd836abd7035b716631b6838c1e60769fa85
tkaiser <TKaiser@chip.de>
2021-08-19 14:34:46 +0200

    update readme

--- app/assets/js/components/App.vue
+++ app/assets/js/components/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <h2 class="text-center">Rendered by vue</h2>
+    <h2 class="text-center text-light">You did it! This page is rendered by vue.</h2>
+    <h4 class="text-center">Happy coding!</h4>
+    <h4 class="text-center">:-)</h4>
   </div>
 </template>
 
